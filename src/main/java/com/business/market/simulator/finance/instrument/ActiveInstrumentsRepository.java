de8c4b03d1075023fbead7094a260a7d9caca9f5
fpatula <filip.patula0@gmail.com>
2023-06-06 21:50:46 +0200

    Removed duplicate repository class

--- src/main/java/com/business/market/simulator/finance/instrument/ActiveInstrumentsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.business.market.simulator.finance.instrument;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActiveInstrumentsRepository extends JpaRepository<ActiveInstrument, Long> {
-}
