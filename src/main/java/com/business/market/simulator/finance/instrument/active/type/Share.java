2c4c00f0269a23b7db156d49bd369df25640bbbb
fpatula <filip.patula0@gmail.com>
2023-06-13 23:40:24 +0200

    Updated and cleaned Share class

--- src/main/java/com/business/market/simulator/finance/instrument/active/type/Share.java
+++ src/main/java/com/business/market/simulator/finance/instrument/active/type/Share.java
@@ -2,14 +2,11 @@ package com.business.market.simulator.finance.instrument.active.type;
 
 import com.business.market.simulator.finance.instrument.InstrumentType;
 import com.business.market.simulator.finance.instrument.active.ActiveInstrument;
-import com.business.market.simulator.finance.instrument.active.ActiveInstrumentRepository;
-import com.business.market.simulator.finance.transaction.MarketTransaction;
 import com.business.market.simulator.utils.BigDecimalToStringConverter;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 
@@ -23,31 +20,5 @@ public class Share extends ActiveInstrument {
     public Share() {
         super();
         this.setType(InstrumentType.SHARE);
-    }
-
-    @Override
-    public MarketTransaction buyInstrument() {
-        return null;
-    }
-
-    @Override
-    public MarketTransaction sellInstrument() {
-        return null;
-    }
-
-    @Override
-    public BigDecimal getCurrentMarketValue() {
-        ShareHelper shareHelper = new ShareHelper();
-        return shareHelper.getLowestAskPrice();
-    }
-
-    private class ShareHelper{
-        @Autowired
-        private ActiveInstrumentRepository activeInstrumentRepository;
-
-        public BigDecimal getLowestAskPrice(){
-            return new BigDecimal(activeInstrumentRepository
-                    .getMaxAskPriceByFinancialInstrumentId(getFinancialInstrument().getInstrumentId()));
-        }
     }
 }
