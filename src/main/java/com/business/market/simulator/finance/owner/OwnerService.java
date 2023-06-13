56a6091e4d7ffb7e63dd5b1a86161180f973e0fb
fpatula <filip.patula0@gmail.com>
2023-06-13 23:40:07 +0200

    Updated OwnerType enum, added OwnerRepository and OwnerService with create methods

---
+++ src/main/java/com/business/market/simulator/finance/owner/OwnerService.java
@@ -0,0 +1,30 @@
+package com.business.market.simulator.finance.owner;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Setter(onMethod_ = @Autowired)
+@Service
+public class OwnerService {
+    OwnerRepository ownerRepository;
+
+    public Company createCompany(String name, String equityCapital, String income, String losses, String dividendsPerShare, OwnerType ownerType) {
+        Company newCompany = new Company(ownerType);
+        newCompany.setOwnerName(name);
+        newCompany.setEquityCapital(new BigDecimal(equityCapital));
+        newCompany.setIncome(new BigDecimal(income));
+        newCompany.setLoses(new BigDecimal(losses));
+        newCompany.setDividendsPerShare(new BigDecimal(dividendsPerShare));
+        return ownerRepository.save(newCompany);
+    }
+
+    public Owner createOwner(String name, OwnerType ownerType) {
+        Owner owner = new Owner();
+        owner.setOwnerName(name);
+        owner.setOwnerType(ownerType);
+        return ownerRepository.save(owner);
+    }
+}
