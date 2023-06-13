fd21a5715c5e2b7e9b1e6a1a9e75ae752e837593
fpatula <filip.patula0@gmail.com>
2023-06-13 23:05:35 +0200

    Changed aspect concept for more clear code strucutre

--- src/main/java/com/business/market/simulator/finance/instrument/aspect/Buyable.java
+++ src/main/java/com/business/market/simulator/finance/instrument/aspect/Buyable.java
@@ -1,7 +1,5 @@
 package com.business.market.simulator.finance.instrument.aspect;
 
-import com.business.market.simulator.finance.transaction.MarketTransaction;
-
 public interface Buyable {
-    MarketTransaction buyInstrument();
+
 }
--- src/main/java/com/business/market/simulator/finance/instrument/aspect/Tradeable.java
+++ src/main/java/com/business/market/simulator/finance/instrument/aspect/Tradeable.java
@@ -1,9 +1,4 @@
 package com.business.market.simulator.finance.instrument.aspect;
 
-import java.math.BigDecimal;
-
 public interface Tradeable extends Buyable, Sellable {
-    default BigDecimal getCurrentMarketValue(){
-        return BigDecimal.ZERO;
-    }
 }
