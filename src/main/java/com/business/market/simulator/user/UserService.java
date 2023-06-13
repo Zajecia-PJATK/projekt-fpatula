00b219d1df7e37ed61e078ea1426d204773edab8
fpatula <filip.patula0@gmail.com>
2023-06-13 23:25:15 +0200

    Updated getPortfolioValue in user service and added method for user removal

--- src/main/java/com/business/market/simulator/user/UserService.java
+++ src/main/java/com/business/market/simulator/user/UserService.java
@@ -1,7 +1,7 @@
 package com.business.market.simulator.user;
 
+import com.business.market.simulator.finance.FinanceService;
 import com.business.market.simulator.finance.instrument.active.ActiveInstrument;
-import com.business.market.simulator.finance.instrument.aspect.Tradeable;
 import com.business.market.simulator.finance.transaction.MarketTransaction;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +14,7 @@ import java.util.List;
 @Service
 public class UserService {
     private UserRepository userRepository;
+    private FinanceService financeService;
 
     public boolean userExists(String username) {
         return userRepository.existsByUsername(username);
@@ -23,20 +24,26 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public User removeUser(User user) {
+        user.setDeleted(true);
+        return userRepository.save(user);
+    }
+
     public User getUserByUsername(String username) {
         return userRepository.findUserByUsername(username);
     }
 
-    public BigDecimal getBalance(User user){
+    public BigDecimal getBalance(User user) {
         return user.getBalance();
     }
 
-    public BigDecimal getPortfolioValue(User user){
+    public BigDecimal getPortfolioValue(User user) {
         List<ActiveInstrument> ownedInstruments = user.getOwnedInstruments();
-        return ownedInstruments.parallelStream().map(Tradeable::getCurrentMarketValue).reduce(BigDecimal.ZERO, BigDecimal::add,BigDecimal::add);
+        return ownedInstruments.parallelStream().map(ownedInstrument -> financeService.getMarketValue(ownedInstrument)).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
-    public List<MarketTransaction> userTransactions(User user){
+    public List<MarketTransaction> userTransactions(User user) {
         return user.getUserTransactions();
     }
+
 }
